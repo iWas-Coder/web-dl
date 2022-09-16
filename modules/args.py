d1d2fdef689aa2f85772338f334fd4d7ec0f4a47
Wasym Atieh Alonso <72618577+iWas-Coder@users.noreply.github.com>
2022-09-16 10:48:15 +0200

    Create args.py

---
+++ modules/args.py
@@ -0,0 +1,31 @@
+"""
+Arguments Module
+~~~~~~~~~~~~~~~~
+
+Adding and parsing all arguments, making them accessible.
+"""
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description = "Syncing app between Puppet & CMDB Databases")
+
+args = {}
+url = lambda: args["url"]
+output = lambda: args["output"]
+
+def init_args() -> None:
+    """
+    Initializes all defined arguments.
+    """
+    
+    # === Adding arguments === #
+    parser.add_argument("-u", "--url", help = "MPD file URL", required = True)
+    parser.add_argument("-o", "--output", help = "Output file", required = True)
+    
+    # === Parsing arguments === #
+    parsed_args = parser.parse_args()
+    
+    # === Making arguments accessible through constants === #
+    args["url"] = parsed_args.url
+    args["output"] = parsed_args.output
