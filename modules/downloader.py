d68f48d6f3abfbec18df09e4113736c9270a89b5
iWas-Coder <wasymatieh01@gmail.com>
2022-09-16 13:52:42 +0200

    Developing

--- modules/downloader.py
+++ modules/downloader.py
@@ -8,7 +8,7 @@ Downloader Module
 import os
 import time
 from pwn import *
-from .import assets
+import modules.assets as assets
 
 
 def get_content(url: str):
@@ -16,9 +16,10 @@ def get_content(url: str):
     (...)
     """
     
-    p = log.progress("CDN")
+    pv = log.progress("Download Video")
+    pa = log.progress("Download Audio")
     
-    p.status("Downloading encrypted video files...")
+    pv.status("Downloading encrypted video files...")
     time.sleep(2)
     os.system("yt-dlp " +
               "--external-downloader aria2c " +
@@ -27,9 +28,9 @@ def get_content(url: str):
               "--no-check-certificate " +
              f"-f {assets.VIDEO_ID} \"{url}\" " +
              f"-o \"{assets.CACHE_DIR}/encrypted_video.%(ext)s\" " +
-              ">/dev/null 2>&1")
+              ">/dev/null 2>&1 &")
     
-    p.status("Downloading encrypted audio files...")
+    pa.status("Downloading encrypted audio files...")
     time.sleep(2)
     os.system("yt-dlp " +
               "--external-downloader aria2c " +
@@ -38,14 +39,26 @@ def get_content(url: str):
               "--no-check-certificate " +
              f"-f {assets.AUDIO_ID} \"{url}\" " +
              f"-o \"{assets.CACHE_DIR}/encrypted_audio.%(ext)s\" " +
-              ">/dev/null 2>&1")
+              ">/dev/null 2>&1 &")
     
-    video_has_downloaded = os.path.isfile(f"{assets.CACHE_DIR}/encrypted_video.*")
-    audio_has_downloaded = os.path.isfile(f"{assets.CACHE_DIR}/encrypted_audio.*")
-    if video_has_downloaded and audio_has_downloaded:
-        p.success("Content downloaded successfully")
+    video_path = f"{assets.CACHE_DIR}/encrypted_video.mp4"
+    video_has_downloaded = os.path.isfile(video_path)
+    audio_path = f"{assets.CACHE_DIR}/encrypted_audio.m4a"
+    audio_has_downloaded = os.path.isfile(audio_path)
+    
+    if video_has_downloaded:
+        pv.success(f"Content downloaded! ({video_path})")
+        if audio_has_downloaded:
+            pa.success(f"Content downloaded! ({audio_path})")
+        else:
+            pa.failure("Content not downloaded due to errors!")
+            sys.exit(1)
     else:
-        p.failure("Content not downloaded due to errors")
+        pv.failure("Content not downloaded due to errors!")
+        if audio_has_downloaded:
+            pa.success(f"Content downloaded! ({audio_path})")
+        else:
+            pa.failure("Content not downloaded due to errors!")
         sys.exit(1)
 
 
