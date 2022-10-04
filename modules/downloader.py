e89980dd6449b0b1d6d19badd678bfa0512ee589
iWas-Coder <wasymatieh01@gmail.com>
2022-10-04 10:36:54 +0200

    FIxing and improving

--- modules/downloader.py
+++ modules/downloader.py
@@ -18,7 +18,7 @@ def get_content(url: str):
     
     p = log.progress("Download")
     
-    pv.status("Downloading and merging encrypted files...")
+    p.status("Downloading and merging encrypted files...")
     time.sleep(2)
     os.system("ffmpeg -v quiet -stats" +
              f"-i \"{url}\" " +
@@ -27,6 +27,8 @@ def get_content(url: str):
     
     video_path = f"{assets.CACHE_DIR}/encrypted_video.mp4"
     audio_path = f"{assets.CACHE_DIR}/encrypted_audio.m4a"
+    
+    p.success("Done!")
 
 
 def merge():
